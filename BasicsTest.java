2798185101db3637538d58730bde0e6640a61fb3
Zach <50342393+KKustos@users.noreply.github.com>
2019-06-17 17:08:34 -0500

    Add files via upload

---
+++ BasicsTest.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class BasicsTest {
+    public static void main(String[] args){
+        Basics basics = new Basics();
+        basics.print1To255();
+        basics.printOdd1To255();
+        basics.printSum();
+        int[] array = {1, 3, 5, 7, 9, 13};
+        basics.interateArray(array);
+        basics.findMax(array);
+        basics.getAvg(array);
+        System.out.println(basics.arrayOddNum());
+        System.out.println(basics.greaterThanY(array,4));
+        System.out.println(Arrays.toString(basics.squareValues(array)));
+        System.out.println(Arrays.toString(basics.eliminateNegNums(array)));
+        System.out.println(Arrays.toString(basics.maxMinAvg(array)));
+        System.out.println(Arrays.toString(basics.shiftArray(array)));
+    }
+}
